7b8e32edf9f66d66d16a84aca1757d52acd3ba76
Bruno Yuri Alves de Oliveira <bruno.yuri@localiza.com>
2023-02-21 16:53:48 -0300

    Métodos construtores são chamados quando criamos um objeto usando o operador new. Por meio deles, podemos informar os valores dos campos no momento da criação do objeto; Construtores explícitos são definidos por quem desenvolve e construtores implícitos são criados pelo próprio .NET quando não definimos explicitamente; A palavra reservada static é utilizada para definir um campo ou propriedade como membro de uma classe. Assim conseguimos compartilhar informações com todos os objetos de uma classe.

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,23 +1,101 @@
-﻿using bytebank;
+﻿using bytebank.Contas;
+using bytebank.Titular;
 
-ContaCorrente contaDoAndre = new ContaCorrente();
-contaDoAndre.titular = "André Silva";
-contaDoAndre.numero_Agencia = 15;
-contaDoAndre.conta = "1010-X";
-contaDoAndre.saldo = 100;
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_Agencia = 15;
+//contaDoAndre.conta = "1010-X";
+//contaDoAndre.saldo = 100;
 
-Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+//Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+
+//ContaCorrente contaDoAndre2 = new ContaCorrente();
+//contaDoAndre2.titular = "André Silva";
+//contaDoAndre2.numero_Agencia = 15;
+//contaDoAndre2.conta = "1010-X";
+//contaDoAndre2.saldo = 100;
+
+//Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+
+//contaDoAndre = contaDoAndre2;
+
+//Console.WriteLine(contaDoAndre == contaDoAndre2);
 
 
-ContaCorrente contaDaMaria = new ContaCorrente();
-contaDaMaria.titular = "Maria Souza";
-contaDaMaria.numero_Agencia = 17;
-contaDaMaria.conta = "1010-5";
-contaDaMaria.saldo = 350;
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "Maria Souza";
+//contaDaMaria.numero_Agencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
 
-Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+//Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
 
-contaDoAndre.Transferir(50, contaDaMaria);
+//contaDoAndre.Transferir(50, contaDaMaria);
 
-Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
-Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+//Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente contaDoPedro = new ContaCorrente();
+//Console.WriteLine(contaDoPedro.titular);
+//Console.WriteLine(contaDoPedro.saldo);
+//Console.WriteLine(contaDoPedro.numero_Agencia);
+//Console.WriteLine(contaDoPedro.conta);
+
+//double valor = 300;
+//double valor2 = valor;
+
+//Console.WriteLine(valor==valor2);
+//Console.WriteLine(valor);
+//Console.WriteLine(valor2);
+
+//Cliente cliente = new Cliente();
+//cliente.nome = "Bruno Oliveira";
+//cliente.cpf = "158.353.667-10";
+//cliente.profissao = "Engenheiro da computação";
+
+//ContaCorrente conta = new ContaCorrente();
+//conta.titular = cliente;
+//conta.conta = "1010-X";
+//conta.numero_Agencia = 15;
+//conta.saldo = 100;
+
+//Console.WriteLine("Titular = " + conta.titular.nome);
+//Console.WriteLine("Cpf = " + conta.titular.nome);
+//Console.WriteLine("Profissão = " + conta.titular.profissao);
+//Console.WriteLine("Nº Agência = " + conta.numero_Agencia);
+//Console.WriteLine("Nº Conta = " + conta.conta);
+//Console.WriteLine("Profissão= " + conta.saldo);
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = new Cliente();
+//conta2.titular.nome = "Jaciane";
+//conta2.titular.profissao = "Tester";
+//conta2.titular.profissao = "12345678910";
+//conta2.conta = "9999-X";
+//conta2.numero_Agencia = 18;
+//conta2.saldo = 500;
+
+//Console.WriteLine(conta2.titular.nome);
+
+//ContaCorrente conta3 = new ContaCorrente();
+//conta3.Numero_agencia = 18;
+//conta3.SetSaldo(200);
+//conta3.Conta = "1010-Q";
+
+//Console.WriteLine(conta3.GetSaldo());
+//Console.WriteLine(conta3.Numero_agencia);
+//Console.WriteLine(conta3.Conta);
+
+//ContaCorrente conta4 = new ContaCorrente(18, "1010-X");
+//conta4.SetSaldo(500);
+//conta4.Titular = new Cliente();
+
+//Console.WriteLine(conta4.GetSaldo());
+//Console.WriteLine(conta4.Numero_agencia);
+
+ContaCorrente conta5 = new ContaCorrente(262, "1234-H");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta6 = new ContaCorrente(1910, "1969-LHP");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
